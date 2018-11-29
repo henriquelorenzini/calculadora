6a3b0e1ce0ab432a994aad63c97d915b9768e58b
Henrique Lorenzini <henriquelorenzini33@gmail.com>
2018-11-29 19:02:16 -0200

    centralizando

--- main.js
+++ main.js
@@ -4,23 +4,21 @@ function calculadora() {
   var resultado = 0;
 
   if(document.getElementsByName('operacao')[0].checked){
-    
     resultado = n1 + n2;
   }
+  
   if(document.getElementsByName('operacao')[1].checked){
-    
     resultado = n1 - n2;
   }
+
   if(document.getElementsByName('operacao')[2].checked){
-    
     resultado = n1 * n2;
   }
   if(document.getElementsByName('operacao')[3].checked){
-    
     resultado = n1 / n2;
   }
+
   if(document.getElementsByName('operacao')[4].checked){
-    
     resultado = n1**n2;
   }
   document.getElementById('resultado').value = resultado;
